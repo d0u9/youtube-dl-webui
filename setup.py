b97c3fde3b4c3c99b68ab3bcf146a48630457738
d0u9 <d0u9.su@outlook.com>
2017-05-05 21:22:02 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from setuptools import setup
+
+DESCRIPTION = 'webui for youtube-dl'
+LONG_DESCRIPTION = 'Another webui for youtube-dl, powered by youtube-dl'
+
+setup (
+        name='youtube_dl_webui',
+        version='0.1-dev',
+        packages=['youtube_dl_webui'],
+        license='GPL-2.0',
+        author='d0u9, yuanyingfeiyu',
+        author_email='d0u9.su@outlook.com',
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        entry_points={
+            'console_scripts': [
+                'youtube-dl-webui = youtube_dl_webui:main'
+            ]
+        },
+)
