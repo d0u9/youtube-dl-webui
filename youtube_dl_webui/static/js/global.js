36377c45814bc7dfd83803ed9715a3273269529e
d0u9 <d0u9.su@outlook.com>
2017-05-18 18:31:46 +0800

    wrong static resource place fix

--- youtube_dl_webui/static/js/global.js
+++ youtube_dl_webui/static/js/global.js
@@ -2,8 +2,45 @@ new Vue({
   el: '#videoWrapper',
   data: {
     videoList: [
-      { name: 'Foo' },
-      { name: 'Bar' }
-    ]
+        {name: 'video'},
+        {name: 'video'},
+        {name: 'video'},
+        {name: 'video'},
+        {name: 'video'},
+        {name: 'video'}
+    ],
+    showModal: false,
+    modalData: {
+        'url': ''
+    },
+    showTab: 'status',
+    tablist: [
+            'status',
+            'details',
+            'files',
+            'peers',
+            'options'
+    ],
+    stateCounter: {
+            all: 0,
+            downloading: 0,
+            finished: 0,
+            paused: 0
+    }
+  },
+  components:{
+          'modal': {template: '#modal-template'}
+  },
+  methods:{
+          addTask: function(){
+                this.showModal = false;
+                console.log('add Task');
+          },
+          deleteTask: function(taskID){
+          }
   }
 })
+
+var getTaskList = function(){
+        console.log('hello');
+}
