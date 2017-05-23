53c8ea4870e8b6fc81f4227a0b67b55d1fcfc1a7
yuanyingfeiyu <yuanjing_sky@163.com>
2017-05-23 22:55:51 +0800

    load data

--- youtube_dl_webui/static/js/global.js
+++ youtube_dl_webui/static/js/global.js
@@ -1,46 +1,62 @@
-new Vue({
-  el: '#videoWrapper',
-  data: {
-    videoList: [
-        {name: 'video'},
-        {name: 'video'},
-        {name: 'video'},
-        {name: 'video'},
-        {name: 'video'},
-        {name: 'video'}
-    ],
-    showModal: false,
-    modalData: {
-        'url': ''
-    },
-    showTab: 'status',
-    tablist: [
-            'status',
-            'details',
-            'files',
-            'peers',
-            'options'
-    ],
-    stateCounter: {
-            all: 0,
-            downloading: 0,
-            finished: 0,
-            paused: 0
-    }
-  },
-  components:{
-          'modal': {template: '#modal-template'}
-  },
-  methods:{
-          addTask: function(){
-                this.showModal = false;
-                console.log('add Task');
-          },
-          deleteTask: function(taskID){
-          }
-  }
-})
+var videoDownload = (function (Vue, extendAM){
+        var videoDownload = {};
+        videoDownload.vm = null;
+        videoDownload.tasksData = {
+                headPath: 'http://localhost:5000/',
+                videoList: [],
+                showModal: false,
+                tablist: ['status', 'details', 'files', 'peers', 'options'],
+                showTab: 'status',
+                stateCounter: { all: 0, downloading: 0, finished: 0, paused: 0 },
+                modalData: { url: '' }
+        };
+        videoDownload.updateVm = function(res) {
+                var that = this;
+                if(that.vm){
+                        that.vm.updated();
+                        return false;
+                }
+                that.vm = new Vue({
+                        el: '#videoWrapper',
+                        data: that.tasksData,
+                        components:{
+                              'modal': {template: '#modal-template'}
+                        },
+                        watch:{
+                                stateCounter: function(){
+                                        stateCounter.all = stateCounter.downloading + stateCounter.finished + stateCounter.paused;
+                                }
+                        },
+                        methods: {
+                                addTask: function(){
+                                        var _self = this;
+                                        var url = _self.headPath + 'task';
+                                        Vue.http.post(url, _self.modalData, {emulateJSON: true}).then(function(res){
+                                                _self.showModal = false;
+                                                that.init();
+                                        }, function(err){
+                                                console.log(err);
+                                        });
+                                }
+                        }
+                });
+        };
 
-var getTaskList = function(){
-        console.log('hello');
-}
+        videoDownload.init = function() {
+                var that = this;
+                var url = that.tasksData.headPath + 'task/list';
+                Vue.http.get(url).then(function(res){
+                        var resData = JSON.parse(res.body);
+                        that.tasksData.videoList = resData.detail;
+                        that.tasksData.stateCounter = resData.state_counter;
+                        that.updateVm(res);
+                }, function(err){
+                        console.log(err)
+                });
+        };
+
+        return videoDownload;
+})(Vue, {});
+
+
+videoDownload.init();
