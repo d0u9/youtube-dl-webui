b13eacf854d90012eb66976b48d493e510e11384
d0u9 <d0u9.su@outlook.com>
2017-06-08 16:17:03 +0800

    update config detect

--- youtube_dl_webui/__init__.py
+++ youtube_dl_webui/__init__.py
@@ -9,7 +9,7 @@ from .core import Core
 def getopt(argv):
     parser = ArgumentParser(description='Another webui for youtube-dl')
 
-    parser.add_argument('-c', '--config', metavar="CONFIG_FILE", required=True, help="config file")
+    parser.add_argument('-c', '--config', metavar="CONFIG_FILE", help="config file", default="/etc/youtube-dl-webui.conf")
     parser.add_argument('--host', metavar="ADDR", help="the address server listens on")
     parser.add_argument('--port', metavar="PORT", help="the port server listens on")
 
