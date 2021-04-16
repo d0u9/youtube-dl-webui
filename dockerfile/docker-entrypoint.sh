4e2be44b0fb944db902b8ec0c8988bf4b3d3363b
Frederic LESUR <contact@memiks.fr>
2021-04-16 17:27:19 +0900

    propose a way to upgrade youtube-dl at startup

--- dockerfile/docker-entrypoint.sh
+++ dockerfile/docker-entrypoint.sh
@@ -7,7 +7,11 @@ puid=${PUID:-$(id -g root)}
 conf=${CONF_FILE:-"/config.json"}
 host=${HOST:-"0.0.0.0"}
 port=${PORT:-5000}
+upgrade=${UPGRADE:-"false"}
 
+if [[ "${upgrade}" == "true" ]]; then
+    pip install --upgrade youtube-dl
+fi
 
 if [[ "$*" == python*-m*youtube_dl_webui* ]]; then
     exec gosu $puid:$pgid "$@" -c $conf --host $host --port $port
